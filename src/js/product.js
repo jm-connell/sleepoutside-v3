b9680025ba8d18c55072ae948caee2c33cd34cf3
Ty Saltzgiver <118573450+Saltzmaster@users.noreply.github.com>
2024-10-03 18:26:41 -0600

    stretch team2

--- src/js/product.js
+++ src/js/product.js
@@ -2,9 +2,9 @@ import { setLocalStorage, getLocalStorage, getParam } from './utils.mjs';
 import { findProductById } from './productData.mjs';
 import productDetails from "./productDetails.mjs";
 
-const productID = getParam('product');
+const productId = getParam('product');
 productDetails(productId, ".product-detail");
-console.log(productID);
+// console.log(findProductById(productId));
 
 
 function addProductToCart(product) {
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,20 +1,38 @@
-let productData;
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
-export default async function productDetails(productID, selector) {
+let product = {};
+
+export default async function productDetails(productId, selector) {
     // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
     // once we have the product details we can render out the HTML
     // add a listener to Add to Cart button
+  product = await findProductById(productId);
+  const el = document.querySelector(selector);
+  el.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
+  document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 
 function addProductToCart(product) {
-    let cartArray = getLocalStorage('so-cart')
-    if (!cartArray) {
-      cartArray = [];
-    }
-    cartArray.push(product);
-    setLocalStorage('so-cart', cartArray);
+  let cartArray = getLocalStorage('so-cart')
+  if (!cartArray) {
+    cartArray = [];
   }
+  cartArray.push(product);
+  setLocalStorage('so-cart', cartArray);
+}
 
 function productDetailsTemplate(product) {
-    return `${product.Name}`;
+  return `<h3>${product.Brand.Name}</h3>
+  <h2 class="divider">${product.NameWithoutBrand}</h2>
+  <img
+    class="divider"
+    src="${product.Image}"
+    alt="${product.Name}"
+  />
+  <p class="product-card__price">$${product.FinalPrice}</p>
+  <p class="product__color">${product.Colors[0].ColorName}</p>
+  <p class="product__description">
+  ${product.DescriptionHtmlSimple}
+  </p>`;
 }
