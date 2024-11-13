20e01cc6b5d6913b6007e2aa98816fdbf339e9b1
Ty Saltzgiver <118573450+Saltzmaster@users.noreply.github.com>
2024-11-13 10:12:41 -0700

    fixed discount + product not found

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -3,9 +3,16 @@ import { cartCount } from "./stores.mjs";
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 let product = {};
+let errorMessage = "";
 
 export default async function productDetails(productId, selector) {
   product = await findProductById(productId);
+
+  if (!product || !product.NameWithoutBrand) {
+    errorMessage = "Error 404 Product Not Found";
+    product = {};
+  }
+
   const el = document.querySelector(selector);
   el.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
   document.getElementById("addToCart").addEventListener("click", addToCart);
@@ -22,6 +29,13 @@ function addToCart() {
 }
 
 function productDetailsTemplate(product) {
+  let discount = product.SuggestedRetailPrice - product.FinalPrice;
+  let roundedDiscount = Math.round(discount * 100) / 100;
+
+  if (errorMessage) {
+    return `<p class="error-message">${errorMessage}</p>`;
+  }
+
   return `<h3>${product.Brand.Name}</h3>
   <h2 class="divider">${product.NameWithoutBrand}</h2>
   <img
@@ -29,9 +43,15 @@ function productDetailsTemplate(product) {
     src="${product.Images.PrimaryLarge}"
     alt="${product.Name}"
   />
-  <p class="product-card__price">$${product.FinalPrice}</p>
+  <p class="product-retail_price">Original Price: <s>$${product.SuggestedRetailPrice}</s></p>
+  <p class="product-card__price">Final Price: $${product.FinalPrice}</p>
+  <p class="product_discount">Total Savings: $${roundedDiscount}</p>
   <p class="product__color">${product.Colors[0].ColorName}</p>
   <p class="product__description">
   ${product.DescriptionHtmlSimple}
-  </p>`;
+  </p>
+  <h2 class="divider"></h2>
+  <div class="product-detail__add">
+    <button id="addToCart">Add to Cart</button>
+  </div>`;
 }
